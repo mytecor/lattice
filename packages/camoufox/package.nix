160bbef974a4ee93bcfa15e5bb249058e375f9d1
mytecor <mytecor@gmail.com>
2026-09-22 20:55:31 +0700

    revert(f18): drop patchelfFlags (breaks autoPatchelf interpreter step); diagnose SEGV separately

--- packages/camoufox/package.nix
+++ packages/camoufox/package.nix
@@ -24,10 +24,6 @@ stdenv.mkDerivation {
   };
 
   nativeBuildInputs = [ unzip autoPatchelfHook ];
-  # Firefox uses "relrhack" to manually process relocations from a fixed
-  # offset (nixpkgs firefox-bin sets the same flag); without it the browser
-  # SEGVs in early NSPR init.
-  patchelfFlags = [ "--no-clobber-old-sections" ];
   buildInputs = [
     gtk3
     alsa-lib
